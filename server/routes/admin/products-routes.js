8bdab4656746f24c6ed17d605bf982485983fa95
Ananya Rawat <128620004+Ananyarawa8t@users.noreply.github.com>
2025-01-11 19:08:55 +0530

    Update products-routes.js

--- server/routes/admin/products-routes.js
+++ server/routes/admin/products-routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const  isAdmin = require("../../controllers/auth/isAdmin")
 const {
   handleImageUpload,
   addProduct,
@@ -13,9 +13,9 @@ const { upload } = require("../../helpers/cloudinary");
 const router = express.Router();
 
 router.post("/upload-image", upload.single("my_file"), handleImageUpload);
-router.post("/add", addProduct);
-router.put("/edit/:id", editProduct);
-router.delete("/delete/:id", deleteProduct);
-router.get("/get", fetchAllProducts);
+router.post("/add",isAdmin, addProduct);
+router.put("/edit/:id", isAdmin,editProduct);
+router.delete("/delete/:id",isAdmin, deleteProduct);
+router.get("/get", isAdmin,fetchAllProducts);
 
 module.exports = router;
